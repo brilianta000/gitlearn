9cfd728223e286a7b5d0abb8ce65e09627fa0be6
brilianta000 <brilianta000@github.com>
2026-04-28 08:46:20 +0000

    style: rapikan layout admin.php - tabel buku dengan SB Admin 2

--- admins/admin.php
+++ admins/admin.php
@@ -2,85 +2,125 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>Data Buku - Admin</title>
 
-    <title>SB Admin 2 - Dashboard</title>
-
-    <!-- Custom fonts for this template-->
+    <!-- Fonts & Icons -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
+    <!-- SB Admin 2 CSS -->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
 
 <body id="page-top">
-<?php
 
+<?php
 require_once 'config/database.php';
 require_once 'classes/buku.php';
 
-$db = new Database('db_kampus');
-$buku = new Buku($db->getconn());
-
+$db    = new Database();
+$buku  = new Buku($db->getConnection());
 $result = $buku->getAll();
-
 ?>
 
+<div id="wrapper">
 
-    <div id="wrapper">
-        <?php include 'sidebar.php' ?>
-        <div id="content-wrapper" class="d-flex flex-column">
-            <?php include 'topbar.php' ?>  
+    <!-- Sidebar -->
+    <?php include 'sidebar.php' ?>
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+
+            <!-- Topbar -->
+            <?php include 'topbar.php' ?>
+
+            <!-- Main Content -->
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800">Data Buku</h1>
+                    <a href="create.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                        <i class="fas fa-plus fa-sm text-white-50"></i> Tambah Buku
+                    </a>
+                </div>
+
+                <!-- Tabel Buku -->
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3 d-flex align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">Daftar Buku</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover" width="100%" cellspacing="0">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th width="5%">No</th>
+                                        <th>Judul Buku</th>
+                                        <th>Penulis</th>
+                                        <th width="12%">Tahun Terbit</th>
+                                        <th width="15%" class="text-center">Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php if ($result && $result->num_rows > 0): ?>
+                                        <?php $no = 1; while ($row = $result->fetch_assoc()): ?>
+                                        <tr>
+                                            <td class="text-center"><?= $no++ ?></td>
+                                            <td><?= htmlspecialchars($row['judul_buku']) ?></td>
+                                            <td><?= htmlspecialchars($row['penulis_buku']) ?></td>
+                                            <td class="text-center"><?= htmlspecialchars($row['tahun_terbit']) ?></td>
+                                            <td class="text-center">
+                                                <a href="edit.php?id=<?= $row['id'] ?>" class="btn btn-warning btn-sm">
+                                                    <i class="fas fa-edit"></i> Edit
+                                                </a>
+                                                <a href="delete.php?id=<?= $row['id'] ?>" class="btn btn-danger btn-sm"
+                                                    onclick="return confirm('Yakin hapus buku ini?')">
+                                                    <i class="fas fa-trash"></i> Hapus
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <?php endwhile; ?>
+                                    <?php else: ?>
+                                        <tr>
+                                            <td colspan="5" class="text-center text-muted">Belum ada data buku.</td>
+                                        </tr>
+                                    <?php endif; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- End Main Content -->
+
         </div>
 
-        <thead>
-            <tr>
-                <td>id</td>
-                <td>judul</td>
-                <td>penulis</td>
-                <td>terbit</td>
-                <td>aksi</td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($row = $result->fetch_assoc()):?>
-            <tr>
-                <td><?=  $row['id'];?></td>
-                <td><?=  $row['judul_buku'];?></td>
-                <td><?=  $row['penulis_buku'];?></td>
-                <td><?=  $row['tahun_terbit'];?></td>
-            </tr>
-            <?php endwhile ?>
-        </tbody>
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Admin Panel &copy; <?= date('Y') ?></span>
+                </div>
+            </div>
+        </footer>
+
     </div>
+    <!-- End Content Wrapper -->
 
+</div>
+<!-- End Wrapper -->
 
-
-<!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+<!-- Bootstrap JS -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
